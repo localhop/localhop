30fc3fa3b618938d68f7ce633d5e2e302253d406
zflies <zachflies11@gmail.com>
2014-11-13 12:31:18 -0600

    Add Custom ListView for viewing events
    Add a custom list adapter and UI for viewing events in the
    events list tab.  The UI is not complete, but it provides
    a start on how to create these custom listViews.  The next
    step will be to encapsulate the custom ListView in a swipe
    view so that the user can swipe between past, today, and
    future events.

--- src/com/localhop/Activity_TabEventList.java
+++ src/com/localhop/Activity_TabEventList.java
@@ -1,19 +1,45 @@
 package com.localhop;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.widget.ListView;
 import android.widget.TextView;
 
-/**
- * Created by Flies on 11/10/14.
- */
+import java.util.ArrayList;
+
+
 public class Activity_TabEventList extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab_events_list);
 
-        setContentView(R.layout.main);
+        // Pass context and data to the custom adapter
+        Adapter_EventList adapter = new Adapter_EventList(this, generateTestData());
+
+        // Get ListView from tab_events_list.xml
+        ListView lvEvents = (ListView)findViewById(R.id.lvEvents);
+
+        // Set the List Adapter
+        lvEvents.setAdapter(adapter);
+
+
     } // end of function onCreate()
+
+    private ArrayList<Item_Event> generateTestData(){
+
+        ArrayList<Item_Event> items = new ArrayList<Item_Event>();
+        items.add(new Item_Event("Chipotle", "Attending: You, Michelle, Ryan", "23rd St. Chipotle", "5:00 PM", 5));
+        items.add(new Item_Event("Senior Design Meetup", "Attending: You, Adam, Kendal", "Spahr Library", "7:00 PM", 0));
+        items.add(new Item_Event("Drinks at Brothers", "Attending: Alexander, Greene, Traylor", "Brother's Bar", "8:00 PM", 1));
+        items.add(new Item_Event("Dempsey's Half Pri...", "Attending: You, Michelle, Ryan", "Dempsey's", "9:00 PM", 1));
+        items.add(new Item_Event("Night out on Mass", "Attending: You, Adam, Kendal", "Mass Street", "11:00 PM", 2));
+        items.add(new Item_Event("Fuzzy's Tacos", "Attending: You, Adam, Kendal", "Fuzzy's Tacos", "1:00 AM", 0));
+
+        return items;
+    }
+
 } // end of class TabEventListActivity
--- src/com/localhop/Item_Event.java
+++ src/com/localhop/Item_Event.java
@@ -1,7 +1,69 @@
 package com.localhop;
 
-/**
- * Created by Flies on 11/12/14.
- */
+
+import java.util.Date;
+
 public class Item_Event {
-}
+
+    // Variables subject to change once
+    // DB integration starts
+    private String sAttendees;
+    private String sEventName;
+    private String sLocation;
+    private String sStartTime;
+    private int nNotificationCount;
+
+    public Item_Event(String sEventName, String sAttendees, String sLocation,
+                      String sStartTime, int nNotificationCount) {
+
+        super();
+        this.sEventName = sEventName;
+        this.sAttendees = sAttendees;
+        this.sLocation = sLocation;
+        this.sStartTime = sStartTime;
+        this.nNotificationCount = nNotificationCount;
+
+    } // end of constructor
+
+
+    public String getsAttendees() {
+        return sAttendees;
+    }
+
+    public void setsAttendees(String sAttendees) {
+        this.sAttendees = sAttendees;
+    }
+
+    public String getsEventName() {
+        return sEventName;
+    }
+
+    public void setsEventName(String sEventName) {
+        this.sEventName = sEventName;
+    }
+
+    public String getsLocation() {
+        return sLocation;
+    }
+
+    public void setsLocation(String sLocation) {
+        this.sLocation = sLocation;
+    }
+
+    public String getsStartTime() {
+        return sStartTime;
+    }
+
+    public void settStartTime(String sStartTime) {
+        this.sStartTime = sStartTime;
+    }
+
+    public int getnNotificationCount() {
+        return nNotificationCount;
+    }
+
+    public void setnNotificationCount(int nNotificationCount) {
+        this.nNotificationCount = nNotificationCount;
+    }
+
+} // end of class Item_Event
